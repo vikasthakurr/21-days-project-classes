231b419349b1480f1121f3a81bbb2bd01a004d3e
vikasthakurr <vikaskumar20012001@gmail.com>
2025-11-09 22:34:54 +0530

    project-2 code pushed

---
+++ project-2/index.js
@@ -0,0 +1,51 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  const isLight = document.body.classList.contains("light-theme");
+  toggleBtn.textContent = isLight ? "🌞" : "🌙";
+});
+
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = [
+  "Software Engineer",
+  "Project Manager",
+  "Web Developer",
+  "Category Lead",
+];
+
+const typingDelay = 50;
+const erasingDelay = 50;
+const newTextDelay = 200;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
